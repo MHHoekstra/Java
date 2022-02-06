a16c09e1abc55b8f24e8b8d84e673f5c878792e9
Michel Henrique Hoekstra <michel.h.hoekstra@gmail.com>
2022-02-06 19:56:12 -0300

    Adapter

---
+++ DesignPattens/Structural/Adapter/src/com/mhhoekstra/round/RoundHole.java
@@ -0,0 +1,20 @@
+package com.mhhoekstra.round;
+
+public class RoundHole {
+    private double radius;
+
+    public RoundHole(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public boolean fits(RoundPeg peg) {
+        boolean result;
+        return this.getRadius() >= peg.getRadius();
+    }
+}
+
+
